7bfaa1ac594286787af5c2f27f974979aeef927f
ShijuPJohn <shijupjohn4u@gmail.com>
2023-07-07 16:45:47 +0530

    start adding routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Layout from "./layout/layout";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/home/home";
 
 function App() {
-  return (
-   <>
-       <Layout>
-           <h1>Helloooo......</h1>
-       </Layout>
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                </Routes>
 
-   </>
-  );
+            </Layout>
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.css'
+import {Link} from "react-router-dom"
 
 function Header(props) {
     return (
@@ -10,10 +11,10 @@ function Header(props) {
             </div>
             <nav className={'main-nav'}>
                 <ul>
-                    <li>Home</li>
-                    <li>Login</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
